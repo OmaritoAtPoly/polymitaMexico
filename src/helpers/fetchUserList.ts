b4411f29805808a96ef44694790edb678fe520b4
mact200590 <mact200590@gmail.com>
2020-05-27 15:07:31 -0400

    add-action

--- src/helpers/fetchUserList.ts
+++ src/helpers/fetchUserList.ts
@@ -4,3 +4,7 @@ export const fetchUsersList: Action = {
   method: 'GET',
   endpoint: '/users',
 };
+export const deleteUserAction = (user:any):Action<any> => ({
+  method: 'DELETE',
+  endpoint: '/users?id=' + user.id
+});
