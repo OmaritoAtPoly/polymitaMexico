491b37e924292b19f44ecfcb3d552b4a16d6c1ad
Omar Estrada Diaz <omarito4857@gmail.com>
2020-05-26 20:11:14 -0500

    refactoring fetching Data

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
 
 interface Props {
-        // loading: any,
-        // error: any,
         users: any,
 }
 
-
-
 const UsersList = ({ users }: Props) => {
 
-
-        return (
-                // users.data.map((u:any) => (u.first_name))
-                users.data.map((u: any) => {
-                        return <div key={u.id}>
-                                <span>Nombre: {`${u.first_name} `}  </span>
-                                <span>Apellido: {`${u.last_name} `}  </span>
-                        </div>
-                        // <div>
-                        // <div></div>
-                        //         <div>Nombre: {u.first_name}</div>
-                })
-                // "hello"
-
-        )
+        return <div>
+                <span>Nombre: {`${users.first_name} `}  </span>
+                <span>Apellido: {`${users.last_name} `}  </span>
+                <span>Email: {`${users.email} `}  </span>
+        </div>
 }
-
 
 export default UsersList;
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,77 +1,17 @@
 import React from 'react';
 import { useQuery } from 'react-fetching-library';
-
-import { fetchUsersList } from '../helpers/fetchUserList';
-import { User } from '../types/index';
 import UsersList from '../components/UserList';
-// import UsersList from '.'
-// import { UsersList } from './UsersList';
-
-
+import { fetchUsersList } from '../helpers/fetchUserList';
 
 const Users = () => {
+
         const { loading, payload, error, query } = useQuery(fetchUsersList);
+
         if (loading) return <div>loading...</div>
-        console.log(payload.data[0]);
-        return (<UsersList users={payload} />)
+        if (error) return <div>Error on {error}</div>
 
-        // return (<div>Hello</div>);
-        // return (
-        //         {loading && "loading"}
-
-
-        //   <List style={{ padding: '24px', display: 'flex', flexFlow: 'column' }}>
-        //     {loading && <CircularProgress style={{ margin: '100px auto' }} />}
-
-        //     {error && (
-        //       <Button onClick={onReload} variant="contained" color="secondary">
-        //         Error, click to reload
-        //       </Button>
-        //     )}
-
-        //     {!loading && !error && users && (
-        //       <ListItem>
-        //         <Typography component="span" color="textPrimary">
-        //           You can turn off wi-fi to see errors after fetch :)
-        //         </Typography>
-        //       </ListItem>
-        //     )}
-
-        //     {!loading &&
-        //       !error &&
-        //       users &&
-        //       users.map(user => (
-        //         <Fragment key={user.uuid}>
-        //           <Divider variant="inset" />
-        //           <ListItem>
-        //             <ListItemAvatar>
-        //               <Avatar alt={user.firstName} src={user.avatar} />
-        //             </ListItemAvatar>
-        //             <ListItemText
-        //               primary={`${user.firstName} ${user.lastName}`}
-        //               secondary={
-        //                 <Fragment>
-        //                   <Typography component="span" color="textPrimary">
-        //                     {user.role}
-        //                   </Typography>
-        //                   {user.description}
-        //                 </Fragment>
-        //               }
-        //             />
-        //           </ListItem>
-        //         </Fragment>
-        //       ))}
-
-        //     {!loading && !error && (
-        //       <Button onClick={onReload} variant="contained" color="primary">
-        //         Click to reload
-        //       </Button>
-        //     )}
-        //   </List>
-        // );
-//       };
-      
-
+        const { data: users } = payload;
+        return users.map((u: any) => <UsersList key={u.id} users={u} />);
 };
 
 
