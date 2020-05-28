13e73b11e23b450c06fa456c6e5dd7fb3a7483f2
mact200590 <mact200590@gmail.com>
2020-05-28 14:58:34 -0400

    fix-modal

--- src/components/ModalDialog.tsx
+++ src/components/ModalDialog.tsx
@@ -1,24 +1,26 @@
 
 import React from 'react';
-import Button, { ButtonType } from './Button';
+import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 interface Props {
   title: string
   name: string
   email: string
   lastName: string
-  label: string;
-  typeVariant: ButtonType;
-  fullWidth: boolean
+  label:string
+ 
 }
 
 
-const ModalDialog = ({ name, email, lastName, title, label, typeVariant, fullWidth }: Props) => {
+const ModalDialog = ({ name, email, lastName, title,label  }: Props) => {
+  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
+
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -30,25 +32,39 @@ const ModalDialog = ({ name, email, lastName, title, label, typeVariant, fullWid
 
 
   return (
-    <div>
-      <Button label={label} typeVariant="primary" fullWidth={false} color="primary" onClick={handleClickOpen} />
-      <Dialog
+    <div className={classes.root}>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen} >{label}</Button>
+      <Dialog 
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
         <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Email:{email} <br />
-              Name:{name}   <br />
-              Last Name:{lastName}
+        <DialogContent style={{minWidth:360}} >
+          <DialogContentText  className={classes.content} id="alert-dialog-description">
+            <text>Email:{email}</text>
+            <text>Name:{name}</text>
+           <text> Last Name:{lastName}</text>
           </DialogContentText>
         </DialogContent>
       </Dialog>
     </div>
   );
 }
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "10%",
+      backgroundColor: theme.palette.background.paper,
+    }, 
+    content:{
+      display:"flex",
+     flexDirection:"column"
+    }
+  })
+);
 
 export default ModalDialog
