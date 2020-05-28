2aca73ab6baa68e4e7b75285f9e1f21c2bb01734
Omar Estrada Diaz <omarito4857@gmail.com>
2020-05-28 04:22:06 -0500

    before t o change X by trash icon

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
 import { makeStyles, createStyles } from '@material-ui/styles';
+import Alert from '@material-ui/lab/Alert';
 
 interface Props {
     name: string;
@@ -10,13 +11,16 @@ interface Props {
 export const UserItem = ({ name, job }: Props) => {
     const classes = useStyles()
     return (
-        <div className={classes.user} >
-            <Typography variant="h4" color="primary" component="h4">
-                {name}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-                {job}
-            </Typography>
+        <div className={classes.container}>
+            <div className={classes.user} >
+                <Typography variant="h4" color="primary" component="h4">
+                    {name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {job}
+                </Typography>
+            </div>
+            <div onClick={() => console.log( name + ' ' + job)}>X</div>
         </div>
     )
 }
@@ -25,6 +29,13 @@ const useStyles = makeStyles({
     user: {
         padding: '.5rem',
         border: '1px solid #ccc',
-        margin: '1rem'
+        margin: '1rem',
+        width: '80%'
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
     }
 });
