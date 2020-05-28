ac961df240b92d4f655887cb9e838b2a75bfdc85
Omarito <44213559+OmaritoAtPoly@users.noreply.github.com>
2020-05-27 21:59:44 -0400

    Form/add user (#5)
    * create AddUserForm
    
    * validate form
    
    * resetForm action added and publishing on PR
    
    * refactoring on codeReview
    
    * refactoring and pulling
    
    Co-authored-by: Yanier Alfonso <48162982+elyanio@users.noreply.github.com>
    Co-authored-by: Asiel Alonso <Asiel Hextiandro>

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route, Redirect } from "react-router-dom";
 import Login from '../pages/Login';
 import Users from '../pages/Users';
+import { AddUserForm } from '../AddUserForm';
 import Dashboard from '../pages/Dashboard';
 
 export const Router = () => {
@@ -10,6 +11,7 @@ export const Router = () => {
             {/* <Route exact path="/" component={Login} /> */}
             <Route exact path="/" component={Dashboard}/>
             <Route path="/users" component={Users} />
+            <Route path="/addUsers" component={AddUserForm} />
         </Switch>
     )
 }
