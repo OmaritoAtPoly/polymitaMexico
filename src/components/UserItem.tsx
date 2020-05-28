5e067a011fe5c985b8c040079ca23d6575d96902
Asiel Alonso <Asiel Hextiandro>
2020-05-28 01:21:37 -0400

    create User itemm

---
+++ src/components/UserItem.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/styles';
+
+interface Props {
+    name: string;
+    job: string;
+}
+
+export const UserItem = ({ name, job }: Props) => {
+    const classes = useStyles()
+    return (
+        <div className={classes.user} >
+            <Typography variant="h4" color="primary" component="h4">
+                {name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                {job}
+            </Typography>
+        </div>
+    )
+}
+
+const useStyles = makeStyles({
+    user: {
+        width: '100%',
+        padding: '1rem',
+        border: '1px solid',
+        borderColor: '#ccc',
+        margin: '1rem'
+    }
+});
--- src/pages/createUser.tsx
+++ src/pages/createUser.tsx
@@ -2,43 +2,31 @@ import React, { useEffect } from 'react';
 import { AddUserForm, UserInput } from '../AddUserForm';
 import { useMutation } from 'react-fetching-library';
 import { addUserAction } from '../helpers/fetchUserList';
+import { UserItem } from '../components/UserItem';
 
 
 const CreateUsers = () => {
         const { loading, payload, mutate, error, reset, abort } = useMutation(addUserAction)
 
+        // const showUser = async () => {
+        //         // if (payload) await document.write(payload.name)
+        //         if (payload)
+        //                 return payload.name;
+        // }
 
-
-        const showUser = async () => {
-                // if (payload) await document.write(payload.name)
-                if(payload)
-                return payload.name;
-        }
-
-        useEffect(() => { showUser() }
-                , [payload]);
-
+        // useEffect(() => { showUser() }
+        //         , [payload]);
 
         const handleSubmit = async (formValues: UserInput) => {
-
-                const { error: mutateError } = await mutate(formValues);
-
-                if (mutateError) {
-                        console.log(error);
-                }
-
-                console.log(showUser());
+                await mutate(formValues);
+                // console.log(showUser());
                 // setUsers(values);
                 // return <AddUserForm loading={loading} handleSubmit={} />
         }
-        
-        if (loading) return <div>loading</div>
-        return <AddUserForm loading={false} error={'error'} handleSubmit={handleSubmit} />
+        return <UserItem name={'Asio'} job={'Programmer'} />
+        // if (loading) return <div>loading</div>
+        // if (error) return <div>Error</div>
+        // if (payload) return <div>{payload.name}</div>
+        // return <AddUserForm loading={false} error={'error'} handleSubmit={handleSubmit} />
 }
-
-
-
-
-
-
 export default CreateUsers;
