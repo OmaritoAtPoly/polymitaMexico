352c0ea39d31c63922b61ff7cae7405bea927578
Asiel Alonso <Asiel Hextiandro>
2020-05-28 12:35:17 -0400

    solve conflictw

--- src/pages/DeleteUser.tsx
+++ src/pages/DeleteUser.tsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import { useBulkMutation } from 'react-fetching-library';
-import deleteUserAction from '../helpers/deleteUser'
 import DeleteUserForm from '../components/DeleteUserForm';
+import { deleteUserAction } from '../helpers/fetchUserList';
 
 export const DeleteUserFormContainer = () => {
-  
-  
-  
   const { loading, responses, mutate, reset, abort } = useBulkMutation(deleteUserAction);
-  
-    const handleSubmit = async (selectedUsers:any) => {
-  
-      const  responses= await mutate(selectedUsers);
-  
-      const errors = responses.filter((response:any) => response).map((response:any) => response.error);
-  
-  
-      if (errors.length > 0) {
-        //show ie. notification
-      }
-       console.log(`everything goes well`)
+
+
+  const handleSubmit = async (selectedUsers: any) => {
+
+    const responses = await mutate(selectedUsers);
+
+    const errors = responses.filter((response: any) => response).map((response: any) => response.error);
+
+
+    if (errors.length > 0) {
+      //show ie. notification
     }
-  
-    return <DeleteUserForm  onSubmit={handleSubmit} />;
-  };
+    console.log(`everything goes well`)
+  }
+
+  return <DeleteUserForm onSubmit={handleSubmit} />;
+};
