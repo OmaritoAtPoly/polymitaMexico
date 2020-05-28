f5294bc9aa1099d202fbb97b9d4f4d1e5ba72737
Asiel Alonso <Asiel Hextiandro>
2020-05-28 02:15:40 -0400

    cretae user

--- src/pages/createUser.tsx
+++ src/pages/createUser.tsx
@@ -1,37 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AddUserForm, UserInput } from '../AddUserForm';
 import { useMutation } from 'react-fetching-library';
 import { addUserAction } from '../helpers/fetchUserList';
 import { UserItem } from '../components/UserItem';
 import Container from '../atoms/Container';
+import ListLayout from '../components/ListLayout';
+import { margin } from 'polished';
 
 
 const CreateUsers = () => {
+        const [users, setUser] = useState<any[]>([])
         const { loading, payload, mutate, error, reset, abort } = useMutation(addUserAction)
 
-        // const showUser = async () => {
-        //         // if (payload) await document.write(payload.name)
-        //         if (payload)
-        //                 return payload.name;
-        // }
+        const addUser = async () => {
+                if (payload) {
+                        setUser([...users, { name: payload.name, job: payload.job }])
+                }
+        }
 
-        // useEffect(() => { showUser() }
-        //         , [payload]);
+        useEffect(() => {
+                addUser()
+        }, [payload]);
 
         const handleSubmit = async (formValues: UserInput) => {
                 await mutate(formValues);
-                // console.log(showUser());
-                // setUsers(values);
-                // return <AddUserForm loading={loading} handleSubmit={} />
         }
         return (
                 <Container>
-                        <UserItem name={'Asio'} job={'Programmer'} />
+                        <div style={{ display: 'flex' }} >
+                                <div style={{ width: '40%', minHeight: '50vp', padding: '.5rem', border: '1px solid #ccc', marginTop: '2rem' }} >
+                                        <ListLayout>
+                                                {users.map((user: any, index: number) => (
+                                                        <UserItem key={index} name={user.name} job={user.job} />
+                                                ))}
+                                        </ListLayout>
+
+                                </div>
+                                <AddUserForm loading={loading} error={'error'} handleSubmit={handleSubmit} />
+                        </div>
+
                 </Container>
         )
-        // if (loading) return <div>loading</div>
-        // if (error) return <div>Error</div>
-        // if (payload) return <div>{payload.name}</div>
-        // return <AddUserForm loading={false} error={'error'} handleSubmit={handleSubmit} />
 }
 export default CreateUsers;
